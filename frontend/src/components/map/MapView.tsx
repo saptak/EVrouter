cfa2605dc09417332596d4e111f42f23bf6ded14
Saptak Sen <saptak@gmail.com>
2025-05-01 18:47:24 -0700

    fix frontend

--- frontend/src/components/map/MapView.tsx
+++ frontend/src/components/map/MapView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polyline, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { RouteResponse } from '../../types/route';
@@ -6,22 +6,23 @@ import { decodePolyline } from '../../utils/mapUtils';
 import L from 'leaflet';
 
 // Fix the marker icon issue in Leaflet
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
-
-let DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41]
+// Use a workaround for the missing marker icons in Leaflet
+// @ts-ignore - Leaflet's typings are not accurate here
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
 });
+
+const DefaultIcon = new L.Icon.Default();
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
 // Custom charging station icon
 const chargingIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
-  shadowUrl: iconShadow,
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41]
 });
@@ -33,18 +34,18 @@ interface MapViewProps {
 const MapView: React.FC<MapViewProps> = ({ route }) => {
   const defaultCenter: [number, number] = [40.7128, -74.0060]; // New York City
   const defaultZoom = 5;
-  
+
   return (
-    <MapContainer 
-      center={defaultCenter} 
-      zoom={defaultZoom} 
+    <MapContainer
+      center={defaultCenter}
+      zoom={defaultZoom}
       style={{ height: '100%', width: '100%' }}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      
+
       {route && <RouteLayer route={route} />}
     </MapContainer>
   );
@@ -56,44 +57,44 @@ interface RouteLayerProps {
 
 const RouteLayer: React.FC<RouteLayerProps> = ({ route }) => {
   const map = useMap();
-  
+
   useEffect(() => {
     if (route && route.route_segments.length > 0) {
       // Get all coordinates for bounds
       const bounds: L.LatLngBounds = new L.LatLngBounds([]);
-      
+
       route.route_segments.forEach(segment => {
         // Add start and end locations to bounds
         bounds.extend([segment.start_location.latitude, segment.start_location.longitude]);
         bounds.extend([segment.end_location.latitude, segment.end_location.longitude]);
-        
+
         // Add decoded polyline points to bounds if available
         if (segment.polyline) {
           const points = decodePolyline(segment.polyline);
           points.forEach(point => bounds.extend([point[0], point[1]]));
         }
       });
-      
+
       // Fit map to bounds with padding
       map.fitBounds(bounds, { padding: [50, 50] });
     }
   }, [route, map]);
-  
+
   if (!route || route.route_segments.length === 0) {
     return null;
   }
-  
+
   return (
     <>
       {route.route_segments.map((segment, index) => {
         // Decode polyline for the segment
-        const positions = segment.polyline 
+        const positions = segment.polyline
           ? decodePolyline(segment.polyline)
           : [
               [segment.start_location.latitude, segment.start_location.longitude],
               [segment.end_location.latitude, segment.end_location.longitude]
             ];
-        
+
         return (
           <React.Fragment key={index}>
             {/* Start location marker - only for first segment */}
@@ -104,9 +105,9 @@ const RouteLayer: React.FC<RouteLayerProps> = ({ route }) => {
                 </Popup>
               </Marker>
             )}
-            
+
             {/* End location marker */}
-            <Marker 
+            <Marker
               position={[segment.end_location.latitude, segment.end_location.longitude]}
               icon={segment.is_charging_stop ? chargingIcon : DefaultIcon}
             >
@@ -122,10 +123,10 @@ const RouteLayer: React.FC<RouteLayerProps> = ({ route }) => {
                 )}
               </Popup>
             </Marker>
-            
+
             {/* Route line */}
             {positions.length > 0 && (
-              <Polyline 
+              <Polyline
                 positions={positions as [number, number][]}
                 color={segment.is_charging_stop ? 'green' : 'blue'}
                 weight={4}
